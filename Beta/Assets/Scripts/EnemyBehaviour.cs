f33e44347efa456182cd243ceef8fec1f442c466
Sugandha9140 <ss9140@nyu.edu>
2017-07-25 17:53:53 +0400

    Removed filed from gitignore

---
+++ Beta/Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour {
+
+	public float speed = 25f;
+	//public GameObject target;
+	Vector3 direction;
+	GameObject enemySpaceship;
+
+
+	// Use this for initialization
+	void Start () {
+		enemySpaceship = GameObject.FindGameObjectWithTag ("enemy");
+		//direction = GetComponentInParent<NewEnemySpawning> ().DestinationFinder (enemySpaceship);
+		//direction = target.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position != direction) {
+			transform.position = transform.position + direction.normalized * speed * Time.deltaTime;
+		}
+	}
+}
